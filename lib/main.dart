363bf64c96b63e7bca5330c844be94d50aca987f
Eric Kisee <erickiseemulwa@gmail.com>
2021-06-07 18:34:24 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+void main () {
+  runApp(
+    FriendlyChatApp(),
+  );
+}
+
+class FriendlyChatApp extends StatelessWidget {
+
+  @override
+  Widget build (BuildContext context){
+    return MaterialApp (
+      title: 'Friendly Chat',
+      home: ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+
+  final _textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold (
+      appBar : AppBar(title: Text('Friendly Chat')),
+      body: _buildTextComposer(),
+
+    );
+  }
+
+  Widget _buildTextComposer () {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container (
+        margin: EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: [
+            Flexible(
+              child: TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+                decoration: InputDecoration.collapsed( hintText: 'Send a message'),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                icon: const Icon(Icons.send),
+                onPressed: ()=> _handleSubmitted(_textController.text)
+              )
+            ),
+          ],
+        ),
+      )
+    );
+    
+  }
+
+  void _handleSubmitted (String text) {
+    _textController.clear();
+  }
+
+} 
